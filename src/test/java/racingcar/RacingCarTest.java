3e3ac4e9399c6b36694d19981973a88b4c290456
SanghyunLee <lgw9736@naver.com>
2024-12-28 14:37:05 +0900

    test: RacingCar 테스트 코드 작성
    - 6자 이상이면 에러, 그 이하는 통과

---
+++ src/test/java/racingcar/RacingCarTest.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+import racingcar.domain.RacingCar;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RacingCarTest {
+
+    @Test
+    void testRacingCar_ThrowExceptionOfLengthOfCarName() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new RacingCar("사과자두포도");
+        });
+    }
+
+    @Test
+    void testRacingCar_NoExceptionForValidNam() {
+        new RacingCar("사과자두배");
+    }
+}
