1c37b8591b0109e451b3aeaaf21002450f0721e5
SanghyunLee <lgw9736@naver.com>
2024-12-31 18:50:52 +0900

    chore: ErrorMessage
    - 상수 이름에서 ERROR_MESSAGE는 중복되어서 제거

--- src/main/java/racingcar/constant/ErrorMessage.java
+++ src/main/java/racingcar/constant/ErrorMessage.java
@@ -1,8 +1,9 @@
 package racingcar.constant;
 
 public class ErrorMessage {
-    public static final String CAR_NAME_LENGTH_ERROR_MESSAGE = "자동차 이름이 5글자가 넘어갑니다.";
-    public static final String NULL_ERROR_MESSAGE = "입력된 값이 없습니다.";
-    public static final String CAR_NAME_IS_NULL_ERROR_MESSAGE = "자동차 이름에 빈 값이 포함되어 있습니다.";
-    public static final String CAR_NAME_IS_DUPLICATE_ERROR_MESSAGE = "자동차 이름에 중복되는 값이 있습니다.";
+    public static final String CAR_NAME_IS_OVER_MAX_LENGTH = "자동차 이름이 5글자가 넘어갑니다.";
+    public static final String NULL_VALUE = "입력된 값이 없습니다.";
+    public static final String CAR_NAME_IS_NULL = "자동차 이름에 빈 값이 포함되어 있습니다.";
+    public static final String CAR_NAME_IS_DUPLICATE = "자동차 이름에 중복되는 값이 있습니다.";
+    public static final String IS_NOT_NUMBER = "입력된 값이 숫자가 아닙니다.";
 }
