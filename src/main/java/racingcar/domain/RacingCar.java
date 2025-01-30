640fdee2c21297d394a0288e7b65b0792d38641a
SanghyunLee <lgw9736@naver.com>
2025-01-30 20:34:29 +0900

    fix: 빈 문자열 예외 검증 로직 추가
    - RacingCar 생성 시 빈 문자열 입력 시 예외가 발생하지 않던 문제 수정

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -14,7 +14,7 @@ public class RacingCar {
     }
 
     private void validate(String name) {
-        if(name.length() >= 6) {
+        if(name.length() >= 6 || name.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.CAR_NAME_IS_OVER_MAX_LENGTH);
         }
     }
